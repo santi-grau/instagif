c85bdb24585f417457642afd974ad220b7d3dc24
santi <santi@codekunstler.com>
2014-07-08 15:06:28 +0200

    init

---
+++ web.js
@@ -0,0 +1,16 @@
+// web.js
+console.log("WWWWWWWWWWWWWWWWWWWWWWWWWW");
+console.log("WWWWWWWWWWWWWWWWWWWWWWWWWW");
+console.log("WWWWWWWWWWWWWWWWWWWWWWWWWW");
+
+var express = require("express");
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
