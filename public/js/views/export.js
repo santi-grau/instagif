39065d8827a9d26357679e3dfabe96e68ee43be3
santi <santi@codekunstler.com>
2014-07-10 12:47:35 +0200

    testing gif speed

--- public/js/views/export.js
+++ public/js/views/export.js
@@ -12,19 +12,25 @@ define(
 				'click #backbut' : 'back'
 			},
 			initialize: function(){
-				this.first = true;
-				//window.app.model.bind('change:screen', _.bind(this.flicker,this));
+				this.count = 0;
+				this.interval = null;
+				window.app.model.bind('change:screen', _.bind(this.flicker,this));
 			},
 			back: function(){
 				window.app.model.set('screen', 'select')
 			},
 			flicker: function(){
 				if(window.app.model.get('screen') == 'export'){
-					this.first = !this.first;
-					//console.log(this.first)
-					//_.delay(this.flicker,1000)
+					this.interval = setInterval(_.bind(this.swap, this), 100)
+				}else{
+					clearInterval(this.interval)
 				}
-			}	
+			},
+			swap: function(){
+				if(this.count < window.app.model.get('images').length-1) this.count += 1;
+				else this.count = 0;
+				this.$('#preview').css('background-image',  'url('+window.app.model.get('images')[this.count]+')');
+			}
 		});
 		return Export;
 	}
